a4a749ecba04bea44b2e9384243f32753284386c
DESKTOP-O7V0JEH\kumsi <kumsihyen@gmail.com>
2024-11-28 14:27:58 +0900

    펠월드에셋추가

---
+++ Unpack/Source/Unpack/UnpackGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UnpackGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UNPACK_API AUnpackGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
